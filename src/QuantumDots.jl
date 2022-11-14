cfffd93add9f379ac2f1502fb1c84c1eda91a1de
Viktor Svensson <svensson.viktor.91@gmail.com>
2022-11-14 21:08:28 +0100

    all fermions are now specified by compile time symbols

--- src/QuantumDots.jl
+++ src/QuantumDots.jl
@@ -1,8 +1,8 @@
 module QuantumDots
 using LinearAlgebra, SparseArrays
 
-export bits,FermionBasis,FermionBasisState,State,focknbr,chainlength, Fermion,
-    CreationOperator, amplitude, species,states, jwstring,focknbrs,FermionState 
+export bits,FermionBasis,State,focknbr,chainlength, Fermion,
+    FermionCreationOperator, amplitude, species,states, jwstring,focknbrs,FermionState 
 
 include("fock.jl")
 include("operators.jl")
--- src/fock.jl
+++ src/fock.jl
@@ -8,61 +8,34 @@ focknbr(bits::Union{BitVector,Vector{Bool}}) = mapreduce(nb -> nb[2] * 2^(nb[1]-
 focknbr(sites::Vector{<:Integer}) = mapreduce(site -> 2^(site-1),+, sites)
 focknbr(sites::Vector{<:Integer},cell_length, species_index=1) = mapreduce(site->2^(digitposition(site,cell_length,species_index)-1),+, sites)
 bits(s::Integer,N) = digits(Bool,s, base=2, pad=N)
-# bits(s::Integer,N) = BitVector(digits(Bool,s, base=2, pad=N)) #more allocations
 
+struct Fermion{ID} end
+struct FermionBasis{IDs} <: AbstractBasis end
+Fermion(id) = Fermion{id}()
+Fermion(args...) = Fermion{args}()
+FermionBasis(N::Integer) = FermionBasis{ntuple(i->(DEFAULT_FERMION_SYMBOL,i),N)}()
+FermionBasis(id) = FermionBasis{id}()
 
-struct FermionBasis{S} <: AbstractBasis end
-FermionBasis() = FermionBasis{(DEFAULT_FERMION_SYMBOL,)}()
-FermionBasis(s::Symbol) = FermionBasis{(s,)}()
-species(::FermionBasis{S}) where S = S
-species(::Type{FermionBasis{S}}) where S = S
-
-struct Fermion{S}
-    site::Int
-end
-species(::Fermion{S}) where S = S
-
-struct FermionBasisState{S,M} 
-    focknbr::Int
-    chainlength::Int
-    FermionBasisState{S}(f::Integer,c::Integer) where S = new{S,length(S)}(f,c)
-end
-FermionBasisState(focknbr::Integer,length::Integer,::FermionBasis{S}) where S = FermionBasisState{S}(focknbr,length)
-FermionBasisState{S}(bits::Union{BitVector,Vector{Bool}}) where S = FermionBasisState{S}(focknbr(bits),length(bits))
-focknbr(state::FermionBasisState)  = state.focknbr
-bits(state::FermionBasisState{S}) where S = bits(focknbr(state),length(S)*chainlength(state))
-species(::FermionBasisState{S}) where S = S
-
-function FermionBasisState{S}(sites,chainlength) where S
-    fn = sum(ss->focknbr(ss[2],length(S),cellindex(Val(ss[1]),Val(S))),sites)
-    FermionBasisState{S}(fn,chainlength)
-end
-FermionBasisState(sites,chainlength,::FermionBasis{S}) where S = FermionBasisState{S}(sites,chainlength)
-
-
-@inline @generated function cellindex(::Val{S},::Val{SS}) where {S,SS}
-    idx = findfirst(y->y==S,SS)
+FermionBasis(chainlength::Integer,species) = FermionBasis{Tuple(map(s->Symbol(s...),Base.product(species,1:chainlength)))}()
+FermionBasis(chainlength::Integer,species::Symbol) = FermionBasis{ntuple(i->Symbol(species,i),chainlength)}()
+nbr_of_fermions(::FermionBasis{S}) where S = length(S)
+siteindex(::Fermion{ID},::FermionBasis{IDs}) where {ID,IDs} = siteindex(Val(ID),Val(IDs))
+@inline @generated function siteindex(::Val{ID},::Val{IDs}) where {ID,IDs}
+    idx = findfirst(y->y==ID,IDs)
     :($idx)
 end
 
-"""
-chainlength(s)
-
-Gives the number of dots in the chain
-"""
-chainlength(s::FermionBasisState) = s.chainlength
-
-function Base.pairs(s::FermionBasisState{S}) where S
-    # N = chainlength(s)
-    M = length(S)
-    b = bits(s)
-    ntuple(n->S[n]=>b[n:M:end],M)
-end
-function Base.show(io::IO, state::FermionBasisState{S}) where {S}
-    println(io,"FermionBasisState{$S}")
-    for (species, bits) in pairs(state)
-        print(io,":",species," => ")
-        print.(io,Int.(bits))
-        println(io)
-    end
-end
+# function Base.pairs(s::FermionBasisState{S}) where S
+#     # N = chainlength(s)
+#     M = length(S)
+#     b = bits(s)
+#     ntuple(n->S[n]=>b[n:M:end],M)
+# end
+# function Base.show(io::IO, state::FermionBasisState{S}) where {S}
+#     println(io,"FermionBasisState{$S}")
+#     for (species, bits) in pairs(state)
+#         print(io,":",species," => ")
+#         print.(io,Int.(bits))
+#         println(io)
+#     end
+# end
