efaa999dda6407a6e84b881448d2aab3e5893040
Viktor Svensson <svensson.viktor.91@gmail.com>
2022-11-14 16:18:11 +0100

    remove TupleTools dependency

--- src/QuantumDots.jl
+++ src/QuantumDots.jl
@@ -1,6 +1,5 @@
 module QuantumDots
 using LinearAlgebra, SparseArrays
-using TupleTools
 
 export bits,FermionBasis,FermionBasisState,State,focknbr,chainlength, Fermion,
     CreationOperator, amplitude, species,states, jwstring,focknbrs,FermionState 
