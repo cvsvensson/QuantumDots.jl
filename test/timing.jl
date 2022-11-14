6dc6c30209e209bfdacf2bf06163785f3087b16c
Viktor Svensson <svensson.viktor.91@gmail.com>
2022-11-14 17:43:01 +0100

    timing test reorganization

--- test/timing.jl
+++ test/timing.jl
@@ -1,11 +1,18 @@
 #This function compares standard matrix mult vs the bitwise strategy. Similar memory alloc. Bitwise faster at N>6
 function timetest(N)
-    M = rand(2^N,2^N)
     ψrand = rand(FermionState{(:a,),Float64},N)
-    op = CreationOperator(:a,1)
-    M*vec(ψrand)
-    op*ψrand
+    focktime(N,ψrand)
+    densetime(N,ψrand)
+end
+
+function densetime(N,ψrand)
+    M = rand(2^N,2^N)
     @time out1 = M*vec(ψrand)
+    return
+end
+function focktime(N,ψrand)
+    op = CreationOperator(:a,1)
+    op*ψrand
     @time out2 = op*ψrand
-    return (out1,out2)
+    return
 end
