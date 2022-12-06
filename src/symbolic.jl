b0f630ce22d8d282bff6949fe5937e525a486a72
Viktor Svensson <svensson.viktor.91@gmail.com>
2022-12-06 16:08:18 +0100

    avoid world age issue with generated hamiltonian

--- src/symbolic.jl
+++ src/symbolic.jl
@@ -1,5 +1,5 @@
 function generate_fastham(gen,params...)
     ps = [(@variables $p)[1] for p in params]
     m = gen(ps...)
-    eval.(build_function(m,ps))
+    (build_function(m,ps,expression=Val{false}))
 end
