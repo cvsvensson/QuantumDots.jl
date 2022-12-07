1656558094414f5a1fd8f2fe8dbd2e90e6b1f438
Viktor Svensson <svensson.viktor.91@gmail.com>
2022-12-07 09:39:42 +0100

    minor refactoring and performance improvement

--- examples/originalpoormans.jl
+++ examples/originalpoormans.jl
@@ -3,7 +3,7 @@ using QuantumDots
 using LinearAlgebra
 using Plots
 
-function pmm_hamiltonian(particle_ops, delta, tun, eps1, eps2)
+function pmm_hamiltonian(particle_ops, delta, tun, eps1, eps2 = eps1)
     d = particle_ops
     ham_dots = eps1*d[1]'*d[1] + eps2*d[2]'*d[2]
     ham_tun = tun*d[1]'*d[2] + tun*d[2]'*d[1]
@@ -12,20 +12,15 @@ function pmm_hamiltonian(particle_ops, delta, tun, eps1, eps2)
     return ham
 end
 
-function pmm_hamiltonian(particle_ops, delta, tun, eps)
-    pmm_hamiltonian(particle_ops, delta, tun, eps, eps)
-end
-
 function groundindices(basis, vecs, energies)
     parityop = basis*QuantumDots.ParityOperator()*basis
     parities = [QuantumDots.measure(parityop, vec) for vec in vecs]
     evenindices = findall(parity -> parity ≈ 1, parities)
     oddindices = setdiff(1:length(energies), evenindices)
-    return evenindices[1], oddindices[1]
+    return evenindices[1]::Int, oddindices[1]::Int
 end
 
-function majoranapolarization(basis, majoranas, oddstate, evenstate)
-    majoranaswbasis = map(maj -> basis*maj*basis, majoranas)
+function majoranapolarization(majoranaswbasis, oddstate, evenstate)
     w, z = map(majwbasis -> QuantumDots.dot(oddstate, majwbasis, evenstate), majoranaswbasis)
     return (w^2 - z^2)/(w^2 + z^2)
 end
@@ -34,15 +29,16 @@ function plot_gapandmp()
     N = 2
     delta = 1
     tun = delta
-    basis = FermionBasis(N)
+    basis = FermionBasis(N, symbol=:d)
     points = 100
     eps_vec = range(-delta, delta, points)
-    gaps = zeros(points)
-    mps = zeros(points)
+    gaps = zeros(Float64, points)
+    mps = zeros(Float64, points)
     d = particles(basis)
     maj_plus = d[1] + d[1]'
     maj_minus = d[1] - d[1]'
     majoranas = (maj_plus, maj_minus)
+    majoranaswbasis = map(maj -> basis*maj*basis, majoranas)
     for i = 1:points
         ham = pmm_hamiltonian(d, delta, tun, eps_vec[i])
         hamwithbasis = basis*ham*basis
@@ -50,7 +46,7 @@ function plot_gapandmp()
         energies, vecs = eigen(mat)
         even, odd = groundindices(basis, eachcol(vecs), energies)
         gaps[i] = abs(energies[even] - energies[odd])
-        mps[i] = majoranapolarization(basis, majoranas, vecs[:,odd], vecs[:,even])
+        mps[i] = majoranapolarization(majoranaswbasis, vecs[:,odd], vecs[:,even])
     end
     display(plot(eps_vec, [gaps, mps], label=["Gap" "MP"]))
     xlabel!("Dot energies")
