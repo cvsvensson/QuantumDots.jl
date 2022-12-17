bb0a89ed07b7eeb35d4b31bee57cd6f59502d127
Viktor Svensson <svensson.viktor.91@gmail.com>
2022-12-17 12:54:57 +0100

    fix poor mans example

--- examples/originalpoormans.jl
+++ examples/originalpoormans.jl
@@ -13,15 +13,15 @@ function pmm_hamiltonian(particle_ops, delta, tun, eps1, eps2 = eps1)
 end
 
 function groundindices(basis, vecs, energies)
-    parityop = basis*QuantumDots.ParityOperator()*basis
-    parities = [QuantumDots.measure(parityop, vec) for vec in vecs]
+    parityop = parityoperator(basis)
+    parities = [v'parityop*v for v in vecs]
     evenindices = findall(parity -> parity ≈ 1, parities)
     oddindices = setdiff(1:length(energies), evenindices)
     return evenindices[1]::Int, oddindices[1]::Int
 end
 
-function majoranapolarization(majoranaswbasis, oddstate, evenstate)
-    w, z = map(majwbasis -> QuantumDots.dot(oddstate, majwbasis, evenstate), majoranaswbasis)
+function majoranapolarization(majoranas, oddstate, evenstate)
+    w, z = map(majorana -> oddstate'*majorana*evenstate, majoranas)
     return (w^2 - z^2)/(w^2 + z^2)
 end
 
@@ -29,24 +29,20 @@ function plot_gapandmp()
     N = 2
     delta = 1
     tun = delta
-    basis = FermionBasis(N, symbol=:d)
+    d = FermionBasis(1:N)
     points = 100
     eps_vec = range(-delta, delta, points)
     gaps = zeros(Float64, points)
     mps = zeros(Float64, points)
-    d = particles(basis)
     maj_plus = d[1] + d[1]'
     maj_minus = d[1] - d[1]'
     majoranas = (maj_plus, maj_minus)
-    majoranaswbasis = map(maj -> basis*maj*basis, majoranas)
     for i = 1:points
         ham = pmm_hamiltonian(d, delta, tun, eps_vec[i])
-        hamwithbasis = basis*ham*basis
-        mat = Matrix(hamwithbasis)
-        energies, vecs = eigen(mat)
-        even, odd = groundindices(basis, eachcol(vecs), energies)
+        energies, vecs = eigen!(Matrix(ham))
+        even, odd = groundindices(d, eachcol(vecs), energies)
         gaps[i] = abs(energies[even] - energies[odd])
-        mps[i] = majoranapolarization(majoranaswbasis, vecs[:,odd], vecs[:,even])
+        mps[i] = majoranapolarization(majoranas, vecs[:,odd], vecs[:,even])
     end
     display(plot(eps_vec, [gaps, mps], label=["Gap" "MP"]))
     xlabel!("Dot energies")
