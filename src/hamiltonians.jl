d943127cc89827cc0ceca7c8df8bb01991748075
Viktor Svensson <svensson.viktor.91@gmail.com>
2023-03-27 14:47:10 +0200

    include phi

--- src/hamiltonians.jl
+++ src/hamiltonians.jl
@@ -45,7 +45,7 @@ function BD1_hamiltonian(c::FermionBasis{M}; μ, h, t, Δ,Δ1, U, V, θ=0.0, ϕ 
     tϕ = t*sin(θ/2) #dnup*exp(iϕ) - exp(-iϕ)*updn
 
     h1s = (_BD1_1site(cell(j,c); μ = μ + dbias[j],h,Δ,U) for j in 1:N)
-    h2s = (_BD1_2site(cell(j,c), cell(j+1,c); t,tϕ,α,Δasym,Δϕ,V) for j in 1:N-1)
+    h2s = (_BD1_2site(cell(j,c), cell(j+1,c); t,tϕ,α,Δasym,Δϕ,V,ϕ) for j in 1:N-1)
     sum(Iterators.flatten((h1s,h2s)))
 end
 function BD1_hamiltonian_disorder(c::FermionBasis{M}; μs, h, Δ1, t, α, Δ, U, V, θ=0.0, bias=0.0) where M
