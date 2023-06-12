ebdde9ba1efc76f39be6a42611b4990fdc7566fa
Viktor Svensson <svensson.viktor.91@gmail.com>
2023-06-12 19:58:43 +0200

    fiix hamiltonian

--- src/hamiltonians.jl
+++ src/hamiltonians.jl
@@ -110,7 +110,7 @@ function BD1_hamiltonian(c::AbstractBasis; μ, h, t, Δ, Δ1, U, V, θ, ϕ)
     @assert length(cell(1, c)) == 2 "Each unit cell should have two fermions for this hamiltonian"
     N = div(M, 2)
     h1s = (_BD1_1site(cell(j, c); μ=getvalue(μ, j, N), h=getvalue(h, j, N), Δ=getvalue(Δ, j, N), U=getvalue(U, j, N)) for j in 1:N)
-    h2s = (_BD1_2site(cell(j, c), cell(mod1(j + 1, N), c); t=getvalue(t, j, N; size=2), Δ1=getvalue(Δ1, j, N; size=2), V=getvalue(V, j, N; size=2), θϕ1=(getvalue(θ, j, N; size=1), getvalue(ϕ, j, N; size=1)), θϕ2=(getvalue(θ, mod1(j + 1, N), N; size=2), getvalue(ϕ, mod1(j + 1, N), N; size=2))) for j in 1:N)
+    h2s = (_BD1_2site(cell(j, c), cell(mod1(j + 1, N), c); t=getvalue(t, j, N; size=2), Δ1=getvalue(Δ1, j, N; size=2), V=getvalue(V, j, N; size=2), θϕ1=(getvalue(θ, j, N), getvalue(ϕ, j, N)), θϕ2=(getvalue(θ, mod1(j + 1, N), N), getvalue(ϕ, mod1(j + 1, N), N))) for j in 1:N)
     return sum(h1s) + sum(h2s)
 end
 
