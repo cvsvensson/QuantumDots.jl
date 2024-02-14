2ed2c7b194e6fe84546e958bf87de85c36b580e8
Viktor Svensson <svensson.viktor.91@gmail.com>
2024-02-14 11:07:01 +0100

    add test

--- test/runtests.jl
+++ test/runtests.jl
@@ -613,6 +613,7 @@ end
     @test newhams[1] isa Matrix
     @test newhams[2] isa BlockDiagonal
     @test newhams[3] isa BdGMatrix
+    @test all(isnothing(pretty_print(ham(b), b)) for b in bases)
     cache = 0.1 .* newhams
     newhams = map(f -> f[1](0.3), fs)
     map((m, f) -> f[2](m, 0.3), cache, fs)
