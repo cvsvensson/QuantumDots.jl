50895cba4bf1795dd8b6cabf9f10e260634fdffe
Viktor Svensson <svensson.viktor.91@gmail.com>
2024-05-05 19:50:15 +0200

    vectorizer docs

--- src/vectorizers.jl
+++ src/vectorizers.jl
@@ -1,11 +1,33 @@
 
 abstract type AbstractVectorizer end
+"""
+    struct KronVectorizer{T} <: AbstractVectorizer
+
+A struct representing a KronVectorizer, the standard vectorizer where superoperators are formed from kronecker products of operators.
+
+# Fields
+- `size::Int`: The size of the KronVectorizer.
+- `idvec::Vector{T}`: A vector representing the vectorized identity matrix. Saved here because it is useful when normalization is needed for computations in e.g. LindbladSystem.
+"""
 struct KronVectorizer{T} <: AbstractVectorizer
     size::Int
     idvec::Vector{T}
 end
 KronVectorizer(n::Integer, ::Type{T}=Float64) where {T} = KronVectorizer{T}(n, vec(Matrix{T}(I, n, n)))
 
+"""
+    struct KhatriRaoVectorizer{T} <: AbstractVectorizer
+
+A struct representing a Khatri-Rao vectorizer. This vectorizer is used for BlockDiagonal density matrices matrices, where the superoperators respect the block structure.
+
+# Fields
+- `sizes::Vector{Int}`: Vector of sizes.
+- `idvec::Vector{T}`: Vector of identifiers.
+- `cumsum::Vector{Int}`: Vector of cumulative sums.
+- `cumsumsquared::Vector{Int}`: Vector of squared cumulative sums.
+- `inds::Vector{UnitRange{Int}}`: Vector of index ranges.
+- `vectorinds::Vector{UnitRange{Int}}`: Vector of vector index ranges.
+"""
 struct KhatriRaoVectorizer{T} <: AbstractVectorizer
     sizes::Vector{Int}
     idvec::Vector{T}
