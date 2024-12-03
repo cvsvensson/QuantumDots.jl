90da4f11b2a7520ddf862aa6aed4441833a5d9f5
Viktor Svensson <svensson.viktor.91@gmail.com>
2024-12-03 11:25:46 +0100

    generalize wedge product

--- src/wedge.jl
+++ src/wedge.jl
@@ -11,6 +11,7 @@ function wedge(b1::FermionBasis, b2::FermionBasis)
     qn = promote_symmetry(b1.symmetry, b2.symmetry)
     FermionBasis(newlabels; qn)
 end
+wedge(b1::FermionBasis, bs...) = foldl(wedge, bs, init=b1)
 
 promote_symmetry(s1::AbelianFockSymmetry{<:Any,<:Any,<:Any,F}, s2::AbelianFockSymmetry{<:Any,<:Any,<:Any,F}) where {F} = s1.conserved_quantity
 promote_symmetry(::AbelianFockSymmetry{<:Any,<:Any,<:Any,F1}, ::AbelianFockSymmetry{<:Any,<:Any,<:Any,F2}) where {F1,F2} = NoSymmetry()
@@ -29,57 +30,125 @@ end
 
 #TODO: specialize for ::NoSymmetry, where kron and parity operator can be used
 #TODO: Try first permuting, then kron, then permuting back
-"""
-    wedge(v1::AbstractVector{T1}, b1::FermionBasis{M1}, v2::AbstractVector{T2}, b2::FermionBasis{M2}, b3::FermionBasis=wedge(b1, b2)) where {M1,M2,T1,T2}
+# """
+#     wedge(v1::AbstractVector{T1}, b1::FermionBasis{M1}, v2::AbstractVector{T2}, b2::FermionBasis{M2}, b3::FermionBasis=wedge(b1, b2)) where {M1,M2,T1,T2}
 
-Compute the wedge product of two vectors `v1` and `v2` in the fermion basis `b1` and `b2`, respectively. Return a vector in the fermion basis `b3`.
+# Compute the wedge product of two vectors `v1` and `v2` in the fermion basis `b1` and `b2`, respectively. Return a vector in the fermion basis `b3`.
 
-# Arguments
-- `v1::AbstractVector`: The first vector.
-- `b1::FermionBasis`: The fermion basis for `v1`.
-- `v2::AbstractVector`: The second vector.
-- `b2::FermionBasis`: The fermion basis for `v2`.
-- `b3::FermionBasis`: The fermion basis for the resulting vector. Defaults to the wedge product of `b1` and `b2`.
+# # Arguments
+# - `v1::AbstractVector`: The first vector.
+# - `b1::FermionBasis`: The fermion basis for `v1`.
+# - `v2::AbstractVector`: The second vector.
+# - `b2::FermionBasis`: The fermion basis for `v2`.
+# - `b3::FermionBasis`: The fermion basis for the resulting vector. Defaults to the wedge product of `b1` and `b2`.
+# """
+# function wedge(v1::AbstractVector{T1}, b1::FermionBasis{M1}, v2::AbstractVector{T2}, b2::FermionBasis{M2}, b3::FermionBasis=wedge(b1, b2)) where {M1,M2,T1,T2}
+#     M3 = length(b3)
+#     check_wedge_basis_compatibility(b1, b2, b3)
+#     T3 = promote_type(T1, T2)
+#     v3 = zeros(T3, 2^M3)
+#     for f1 in 0:2^M1-1, f2 in 0:2^M2-1
+#         f3 = f1 + f2 * 2^M1
+#         pf = parity(f2)
+#         v3[focktoind(f3, b3)] += v1[focktoind(f1, b1)] * v2[focktoind(f2, b2)] * pf
+#     end
+#     return v3
+# end
+
+
+
+get_fockstates(::FermionBasis{M,<:Any,NoSymmetry}) where {M} = 0:2^M-1
+get_fockstates(b::FermionBasis) = get_fockstates(b.symmetry)
+get_fockstates(sym::AbelianFockSymmetry) = sym.indtofockdict
 """
-function wedge(v1::AbstractVector{T1}, b1::FermionBasis{M1}, v2::AbstractVector{T2}, b2::FermionBasis{M2}, b3::FermionBasis=wedge(b1, b2)) where {M1,M2,T1,T2}
-    M3 = length(b3)
-    check_wedge_basis_compatibility(b1, b2, b3)
-    T3 = promote_type(T1, T2)
-    v3 = zeros(T3, 2^M3)
-    for f1 in 0:2^M1-1, f2 in 0:2^M2-1
-        f3 = f1 + f2 * 2^M1
-        pf = parity(f2)
-        v3[focktoind(f3, b3)] += v1[focktoind(f1, b1)] * v2[focktoind(f2, b2)] * pf
+    wedge(ms::AbstractVector, bs::AbstractVector{<:FermionBasis}, b::FermionBasis=wedge(bs...))
+
+Compute the wedge product of matrices or vectors in `ms` with respect to the fermion bases `bs`, respectively. Return a matrix in the fermion basis `b`, which defaults to the wedge product of `bs`.
+"""
+function wedge(ms::AbstractVector, bs::AbstractVector{<:FermionBasis}, b::FermionBasis=wedge(bs...))
+    T = promote_type(map(eltype, ms)...)
+    dimlengths = map(length ∘ get_fockstates, bs)
+    Nout = prod(dimlengths)
+    if ndims(first(ms)) == 1
+        mout = zeros(T, Nout)
+        return wedge_vec!(mout, ms, bs, b, Val(length(ms)))
+    elseif ndims(first(ms)) == 2
+        mout = zeros(T, Nout, Nout)
+        return wedge_mat!(mout, ms, bs, b, Val(length(ms)))
     end
-    return v3
+    throw(ArgumentError("Only 1D or 2D arrays are supported"))
 end
-"""
-    wedge(m1::AbstractMatrix{T1}, b1::FermionBasis{M1}, m2::AbstractMatrix{T2}, b2::FermionBasis{M2}, b3::FermionBasis=wedge(b1, b2)) where {M1,M2,T1,T2}
-
-Compute the wedge product of two matrices `m1` and `m2` with respect to the fermion bases `b1` and `b2`, respectively. Return a matrix in the fermion basis `b3`, which defaults to the wedge product of `b1` and `b2`.
-
-# Arguments
-- `m1::Union{AbstractMatrix{T1}, UniformScaling{T1}}`: The first matrix.
-- `b1::FermionBasis{M1}`: The fermion basis associated with `m1`.
-- `m2::Union{AbstractMatrix{T2}, UniformScaling{T2}}`: The second matrix.
-- `b2::FermionBasis{M2}`: The fermion basis associated with `m2`.
-- `b3::FermionBasis`: The fermion basis associated with the resulting matrix. (optional)
-"""
-function wedge(m1::Union{AbstractMatrix{T1},UniformScaling{T1}}, b1::FermionBasis{M1}, m2::Union{AbstractMatrix{T2},UniformScaling{T2}}, b2::FermionBasis{M2}, b3::FermionBasis=wedge(b1, b2)) where {M1,M2,T1,T2}
-    M3 = length(b3)
-    check_wedge_basis_compatibility(b1, b2, b3)
-    T3 = promote_type(T1, T2)
-    m3 = zeros(T3, 2^M3, 2^M3)
-    for f1_1 in 0:2^M1-1, f1_2 in 0:2^M2-1
-        f1_3 = f1_1 + f1_2 * 2^M1
-        pf1 = parity(f1_2)
-        for f2_1 in 0:2^M1-1, f2_2 in 0:2^M2-1
-            f2_3 = f2_1 + f2_2 * 2^M1
-            pf2 = parity(f2_2)
-            m3[focktoind(f1_3, b3), focktoind(f2_3, b3)] += m1[focktoind(f1_1, b1), focktoind(f2_1, b1)] * m2[focktoind(f1_2, b2), focktoind(f2_2, b2)] * pf1 * pf2
+function wedge_mat!(mout, ms::AbstractVector, bs::AbstractVector{<:FermionBasis}, b::FermionBasis, ::Val{N}) where {N}
+    Ms = map(nbr_of_fermions, bs)
+    Mout = sum(Ms)
+    shifts = pushfirst!(cumsum(Ms), 0)
+    dimlengths = map(length ∘ get_fockstates, bs)
+    inds::CartesianIndices{N,NTuple{N,Base.OneTo{Int64}}} = CartesianIndices(Tuple(dimlengths))
+    for I in inds
+        TI = Tuple(I)
+        fock1 = map(indtofock, TI, bs)
+        fullfock = mapreduce((M, f) -> 2^M * f, +, shifts, fock1)
+        outind = focktoind(fullfock, b)
+        for I2 in inds
+            TI2 = Tuple(I2)
+            fock2 = map(indtofock, TI2, bs)
+            fullfock2 = mapreduce((M, f) -> 2^M * f, +, shifts, fock2)
+            v = mapreduce((m, b, i1, f1, i2, f2, M) -> m[i1, i2] * phase_factor(f1, f2, M), *, ms, bs, TI, fock1, TI2, fock2, Ms)
+            mout[outind, focktoind(fullfock2, b)] += v * phase_factor(fullfock, fullfock2, Mout)
         end
     end
-    return m3
+    return mout
+end
+function wedge_vec!(mout, ms::AbstractVector, bs::AbstractVector{<:FermionBasis}, b::FermionBasis, ::Val{N}) where {N}
+    Ms = map(nbr_of_fermions, bs)
+    partition = map(_b -> siteindices(collect(keys(_b)), b), bs)
+    U = embedding_unitary(partition, get_fockstates(b))
+    Mout = sum(Ms)
+    @assert length(unique(reduce(vcat, partition))) == Mout
+    shifts = pushfirst!(cumsum(Ms), 0)
+    dimlengths = map(length ∘ get_fockstates, bs)
+    inds::CartesianIndices{N,NTuple{N,Base.OneTo{Int64}}} = CartesianIndices(Tuple(dimlengths))
+    for I in inds
+        TI = Tuple(I)
+        fock1 = map(indtofock, TI, bs)
+        fullfock = mapreduce((M, f) -> 2^M * f, +, shifts, fock1)
+        outind = focktoind(fullfock, b)
+        mout[outind] += mapreduce((i1, m) -> m[i1], *, TI, ms)
+    end
+    return U * mout
+end
+
+# function partition_phase_factor(f, ξ)
+#     phase = 1
+#     for (s, Xs) in enumerate(ξ)
+#         mask = focknbr_from_site_indices(Xs)
+#         masked_f = mask & f
+#         for (r, Xr) in Iterators.drop(enumerate(ξ), s)
+#             for i in Xr
+#                 phase *= (_bit(f, i) ? jwstring_left(i, masked_f) : 1)
+#             end
+#         end
+#     end
+#     return phase
+# end
+function embedding_unitary(ξ, fockstates)
+    #for locally physical algebra, ie only for even operators or states of well-defined parity
+    #if ξ is ordered, the phases are +1. 
+    # Note that the jordan wigner modes are ordered in reverse
+    phases = ones(Int, length(fockstates))
+    for (s, Xs) in enumerate(ξ)
+        mask = focknbr_from_site_indices(Xs)
+        for (r, Xr) in Iterators.drop(enumerate(ξ), s)
+            for i in Xr
+                for (n, f) in zip(eachindex(phases), fockstates)
+                    if _bit(f, i)
+                        phases[n] *= jwstring_right(i, mask & f)
+                    end
+                end
+            end
+        end
+    end
+    return Diagonal(phases)
 end
 
 struct PhaseMap
